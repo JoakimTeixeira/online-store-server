afa5b7753a25b948b3392f1166c3ab90eea28e41
Joakim Teixeira <setokim1@hotmail.com>
2026-01-04 16:38:01 +0000

    Add middlewares for routes with error handling

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 import express from "express";
 
 const app = express();
+
+// Middlewares follow the "Chain of Responsibility" pattern
+
+// Global Middleware: only parses JSON requests
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Hello, World!"));
+// Global Middleware: triggers error
+app.use((req, res, next) => {
+  console.log("will run before any route");
+  try {
+    throw new Error("failing here!");
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Global Middleware: error handler
+app.use((err, req, res, next) => {
+  console.log("something goes wrong");
+  res.status(500).send(err.message);
+});
+
+// Route-based Middleware
+app.use("/users", (req, res, next) => {
+  console.log("will run before users route");
+  next();
+});
+
+app.get("/", (req, res) => {
+  console.log("route / called");
+  res.send("Hello, World!");
+});
+
+app.get("/users", (req, res) => {
+  console.log("route /users called");
+  res.send("Hello users!");
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
